41f18784494b86c1ac4710335432f374fbc0006d
dumeng <dumeng81@outlook.com>
2020-05-07 21:35:46 +0800

    add button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-navigator.bluetooth.requestDevice({
-  filters: [{
-    services: [],
-  }]
-}).then(device => console.log('devices:', device))
+const $ = document.querySelector.bind(document);
+
+const requestButton = $("#request");
+
+requestButton.addEventListener("click", () => {
+  navigator.bluetooth
+    .requestDevice({
+      filters: [
+        {
+          services: [],
+        },
+      ],
+    })
+    .then((device) => console.log("devices:", device));
+});
