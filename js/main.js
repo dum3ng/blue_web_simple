9813f3dedd2a266f1df86a0bb3a39b17cd3218b5
dumeng <dumeng81@outlook.com>
2020-05-08 09:26:28 +0800

    add one more button

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ const requestButton = $("#request");
 
 requestButton.addEventListener("click", () => {
   navigator.bluetooth
-    .requestDevice({
-      filters: [
-        {
-          services: [],
-        },
-      ],
-    })
+    .requestDevice()
     .then((device) => console.log("devices:", device));
+});
+
+const devicesButton = $("#button2");
+
+devicesButton.addEventListener("click", async () => {
+  const devices = await navigator.bluetooth.getDevices();
+  console.log("get devices ", devices);
 });
